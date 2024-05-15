5d5778caa554a963948d6badba1af062c425d9ae
John Loyd Mulit <jlmulit68@gmail.com>
2024-05-15 11:28:16 +0800

    fix: name field

--- database/mysql.go
+++ database/mysql.go
@@ -60,7 +60,7 @@ func (m *Mysql) InsertAdmin(admin *request.AdminModel) (int, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
-	query := "INSERT INTO Admin (email, password, role) VALUES (:email, :password, :role)"
+	query := "INSERT INTO Admin (username, password, role) VALUES (:username, :password, :role)"
 	res, err := m.Conn.NamedExecContext(ctx, query, admin)
 	if err != nil {
 		return 0, err
