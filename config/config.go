79dd44096069d30b88ffd97be3ca10fee5ca8af7
John Loyd Mulit <jlmulit68@gmail.com>
2024-05-15 09:13:40 +0800

    feat: read data from json file

--- config/config.go
+++ config/config.go
@@ -7,11 +7,12 @@ import (
 )
 
 type Config struct {
-	DB_User string
-	DB_Pass string
-	DB_Name string
-	DB_Host string
-	DB_Port string
+	DB_User    string
+	DB_Pass    string
+	DB_Name    string
+	DB_Host    string
+	DB_Port    string
+	SourceData string
 }
 
 func NewEmptyConfig() *Config {
@@ -26,14 +27,16 @@ func (c *Config) LoadConfigFromEnv() error {
 	c.DB_Name = os.Getenv("DB_NAME")
 	c.DB_Host = os.Getenv("DB_HOST")
 	c.DB_Port = os.Getenv("DB_PORT")
+	c.SourceData = os.Getenv("SOURCE_DATA")
 
 	return nil
 }
 
-func (c *Config) LoadConfigWithValues(user, pass, db, host, port string) {
+func (c *Config) LoadConfigWithValues(user, pass, db, host, port, data string) {
 	c.DB_User = user
 	c.DB_Pass = pass
 	c.DB_Name = db
 	c.DB_Host = host
 	c.DB_Port = port
+	c.SourceData = data
 }
--- seeder/seeder.go
+++ seeder/seeder.go
@@ -1,8 +1,12 @@
 package seeder
 
 import (
+	"encoding/json"
+	"os"
+
 	"github.com/BeepLoop/nearbyassist_seeder/config"
 	"github.com/BeepLoop/nearbyassist_seeder/database"
+	"github.com/BeepLoop/nearbyassist_seeder/request"
 )
 
 type Seeder struct {
@@ -18,6 +22,28 @@ func NewSeeder(conf *config.Config, db database.Database) *Seeder {
 }
 
 func (s *Seeder) Seed() error {
-	println("Seeding...")
+	data, err := s.ReadJsonData()
+	if err != nil {
+		return err
+	}
+
+	for _, entry := range data.Data {
+		println(entry.Table)
+	}
+
 	return nil
+}
+
+func (s *Seeder) ReadJsonData() (*request.JsonData, error) {
+	bytes, err := os.ReadFile(s.Config.SourceData)
+	if err != nil {
+		return nil, err
+	}
+
+	data := &request.JsonData{}
+	if err := json.Unmarshal(bytes, data); err != nil {
+		return nil, err
+	}
+
+	return data, nil
 }
