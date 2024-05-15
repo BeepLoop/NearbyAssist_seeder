a6602e7031f1f9daba4979dd23fbbe7a82a5e7bd
John Loyd Mulit <jlmulit68@gmail.com>
2024-05-15 08:22:56 +0800

    feat: flag for running in headless mode

--- cmd/main.go
+++ cmd/main.go
@@ -1,25 +1,38 @@
 package main
 
 import (
+	"flag"
+	"fmt"
+	"os"
+
 	"github.com/BeepLoop/nearbyassist_seeder/config"
 	"github.com/BeepLoop/nearbyassist_seeder/database"
 	"github.com/BeepLoop/nearbyassist_seeder/seeder"
 )
 
 func main() {
-	// Load Config
-	conf := config.NewEmptyConfig()
-	conf.LoadConfigFromEnv()
+	headless := flag.Bool("headless", false, "Run without GUI")
+	flag.Parse()
 
-	// Load Database
-	db := database.NewMysqlDatabase(conf)
-	if err := db.InitConnection(); err != nil {
-		panic("Failed to connect to database, error: " + err.Error())
-	}
+	if *headless {
+		fmt.Println("Running in headless mode...")
 
-	// Load Seeder backend
-	seeder := seeder.NewSeeder(conf, db)
-	if err := seeder.Seed(); err != nil {
-		panic("Failed to seed, error: " + err.Error())
+		// Load Config
+		conf := config.NewEmptyConfig()
+		conf.LoadConfigFromEnv()
+
+		// Load Database
+		db := database.NewMysqlDatabase(conf)
+		if err := db.InitConnection(); err != nil {
+			panic("Failed to connect to database, error: " + err.Error())
+		}
+
+		// Load Seeder backend
+		seeder := seeder.NewSeeder(conf, db)
+		if err := seeder.Seed(); err != nil {
+			panic("Failed to seed, error: " + err.Error())
+		}
+
+		os.Exit(0)
 	}
 }
